b627e6a5585c356de7580adfede840f0aea0f302
Silvio Melo Filho <smfilho@gmail.com>
2020-05-30 23:06:27 -0500

    Minor style updates

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -3,6 +3,9 @@ import { FiLogIn } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
 
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
 import { Container, Content, Background } from './styles';
 
 const SignIn: React.FC = () => (
@@ -12,11 +15,11 @@ const SignIn: React.FC = () => (
       <form>
         <h1>Login</h1>
 
-        <input placeholder="E-mail"/>
+        <Input placeholder="E-mail"/>
 
-        <input type="password" placeholder="Password"/>
+        <Input type="password" placeholder="Password"/>
 
-        <button type="submit">Enter</button>
+        <Button type="submit">Enter</Button>
 
         <a href="forgot">Forgot Password</a>
       </form>
