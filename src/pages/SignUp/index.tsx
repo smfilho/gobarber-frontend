47afae6fb5b791114791b103224b014353bb85b0
Silvio Melo Filho <smfilho@gmail.com>
2021-02-14 22:21:10 -0600

    Added Yup

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
+import * as Yup from 'yup';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -10,9 +11,23 @@ import Button from '../../components/Button';
 import { Container, Content, Background } from './styles';
 
 const SignUp: React.FC = () => {
-  function handleSubmit(data: unknown): void {
-    console.log(data);
-  }
+  const handleSubmit = useCallback(async (data: unknown) => {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Name required'),
+        email: Yup.string()
+          .required('E-mail required')
+          .email('Only valid e-mails'),
+        password: Yup.string().min(6, '6 digit min'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
 
   return (
     <Container>
